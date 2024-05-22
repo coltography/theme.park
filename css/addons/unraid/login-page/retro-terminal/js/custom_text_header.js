c6bfa10130fa153a55657303204fb6aa8d31cf07
coltography <coltography@gmail.com>
2024-05-22 16:20:52 -0700

    Update custom_text_header.js

--- css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
+++ css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
@@ -6,13 +6,12 @@
 // YOU MUST ESCAPE ANY SINGLE BACKSLASHES LIKE SO: \\
 
 let custom_text_header = `
-<div class="custom-text-header"> <pre>
-                                 
-  ____ ______________  __________
- / __ `/ ___/ ___/ _ \/ ___/ ___/
-/ /_/ / /__/ /__/  __(__  |__  ) 
-\__,_/\___/\___/\___/____/____/  
-                                  
+<div class="custom-text-header"> <pre>              __             __            _     
+  ____ ______/ /__________  / /___  ____ _(_)___ 
+ / __ `/ ___/ __/ ___/ __ \/ / __ \/ __ `/ / __ \
+/ /_/ (__  ) /_/ /  / /_/ / / /_/ / /_/ / / / / /
+\__,_/____/\__/_/   \____/_/\____/\__, /_/_/ /_/ 
+                                 /____/          
                                                                  </pre> </div>
 `;
 document.getElementById("login").innerHTML += custom_text_header
